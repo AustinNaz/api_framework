7770026e47bd1ce6f497b5a20de510e6c4a6a329
Austin <austinljnazareth@gmail.com>
2021-10-08 14:51:43 -0400

    Accidentally added in commit

--- routes/scrape/index.ts
+++
@@ -1,8 +0,0 @@
-const Routes = ({ app, folderName: routeName }: Routes) => {
-  app.get(routeName, (req, res, next) => {
-    const { success } = req;
-    success({ res, next, body: "Scrape Scrape!" });
-  });
-};
-
-export default Routes;
